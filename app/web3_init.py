c6d5999c32aaf06f0521b382341e43dcd4d06f85
Ziyuan(Jack) Jiang <48230839+ZiyuanJiang825@users.noreply.github.com>
2022-12-05 00:24:22 -0500

    update web3_config so that the private key will not be exposed

--- app/web3_init.py
+++ app/web3_init.py
@@ -3,11 +3,12 @@ from web3 import Web3
 from web3.gas_strategies.rpc import rpc_gas_price_strategy
 from web3_config import *
 from eth_account import Account
+import os
 
 w3 = Web3(Web3.HTTPProvider(WEB3_URL))
 w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
 
-w3.eth.default_account = Account.from_key(ADMIN_PRIVATE_KEY)
+w3.eth.default_account = Account.from_key(os.environ['ADMIN_PRIVATE_KEY'])
 
 '''
 Send some money from admin account to the contract
